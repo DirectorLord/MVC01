7e613bcb353e934f9b63c5cde051652c280599af
DirectorLord <thelord161203@gmail.com>
2025-08-31 23:50:55 +0300

    upgraded to .NET 9 using the upgrade tool

--- MVC01/Program.cs
+++ MVC01/Program.cs
@@ -3,11 +3,19 @@ namespace MVC01;
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder();
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        #region ConfigureServices
+        //builder.Services.Add<>()
+        #endregion
 
+        //builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
         app.Run();
+        //this version is >NET 7 so maybe its like that and will upgrade to .NET 9
+
+        //upgraded to .NET 9 and i dont see anyy difference
     }
 }
